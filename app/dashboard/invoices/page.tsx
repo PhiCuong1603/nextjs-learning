ca6e811e9292a9a3437d1dd0c2e1ed3bce981078
cuongdo <phcuong1603@gmail.com>
2025-05-23 14:28:11 +0700

    innit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+  console.log('Invoices page');
+  return <p>Invoices Page</p>;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+  console.log('Dashboard page');
+  return <p>Dashboard Page</p>;
+}
